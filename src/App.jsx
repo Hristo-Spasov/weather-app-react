c4f74a963d17e50a08460365cca383c84b305bd7
Trickkyyy <vandark5@gmail.com>
2023-04-06 00:08:16 +0300

    fixing api req http => https

--- src/App.jsx
+++ src/App.jsx
@@ -49,7 +49,7 @@ function App() {
   //* Forecast API
   const getForecast = () => {
     fetch(
-      `http://api.weatherapi.com/v1/forecast.json?key=${FORECAST_KEY}&q=${city}&days=1&aqi=no&alerts=no`
+      `https://api.weatherapi.com/v1/forecast.json?key=${FORECAST_KEY}&q=${city}&days=1&aqi=no&alerts=no`
     )
       .then((res) => res.json())
       .then((data) => setForecast(data));
